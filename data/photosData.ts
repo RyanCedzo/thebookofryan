0d6dcd5bbc16e5f50ae849f2b091b86d1c17467e
RyanCedzo <cedzo2@illinois.edu>
2025-06-13 17:58:00 -0400

    added the rest of the photo data and fixed some typos

--- data/photosData.ts
+++ data/photosData.ts
@@ -58,62 +58,110 @@ const photosData: Photo[] = [
     {
         title: 'Yosemite Vernal Rainbow',
         imgSrc: '/static/images/gallery/yosemiteRainbow.jpg',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "Vernal Falls, Yosemite, CA",
+        date: "April 12, 2025",
+        description: "A small rainbow above the Merced river just beyond Vernal Falls in Yosemite National Park"
     },
     {
         title: 'River Backflip',
         imgSrc: '/static/images/gallery/riverBackflipColdPonds.jpg',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "Hilo, Big Island, HI",
+        date: "February 23, 2025",
+        description: "I have a surprisingly large number of pictures of River doing backflips in cool places"
     },
     {
         title: 'Erie Sunset Splash',
         imgSrc: '/static/images/blog/thumbs/sunsetSplash.jpg',
-        isVertical: false
+        isVertical: false,
+        camera: "Pixel 5",
+        location: "Presque Isle, Erie, PA",
+        date: "November 8, 2021",
+        description: "One of the first photos that helped me fall in love with capturing moments. This was taken near RP15 for those that know"
     },
     {
         title: 'Ventura Cove Jam',
         imgSrc: '/static/images/gallery/venturaCoveJam.png',
-        isVertical: true
+        isVertical: true,
+        camera: "Vivitar PS:35",
+        location: "Ventura Cove, San Diego, CA",
+        date: "March 27, 2025",
+        description: "Such talented friends! Nolan, James, and Wick jamming in Ventura Cove. Wick even taught me some baselines to jam with them. Time for me to pick up some strings and learn to make nice sounds with them."
     },
     {
         title: 'White Sands NP',
         imgSrc: '/static/images/gallery/whiteSands.jpg',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "White Sands National Park, NM",
+        date: "December 8, 2024",
+        description: "Part of my first ever film rolls. Ventured out amongst the dunes in search of the perfect shaded area for the black and white photo."
     },
     {
         title: 'Padres Game',
         imgSrc: '/static/images/gallery/padresGame.png',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "Petco Park, San Diego, CA",
+        date: "April 1, 2025",
+        description: "Guardians were playing the Padres while I was in San Diego so I had to go watch them lose 7-1"
     },
     {
         title: 'Parked Outside Yosemite',
         imgSrc: '/static/images/gallery/carsYosemite.png',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "Wawona, CA",
+        date: "April 12, 2025",
+        description: "J and I parked up on this land outside Yosemite for a week. What a nice place to work and sleep. Toto looking pretty."
     },
     {
         title: 'Thomas + Alyssa',
         imgSrc: '/static/images/gallery/thomasAndAlyssa.jpg',
-        isVertical: true
+        isVertical: true,
+        camera: "Vivitar PS:35",
+        location: "Los Angeles, CA",
+        date: "April 19, 2025",
+        description: "Cutest couple. Still waiting to watch the final 2 episodes of Game of Thrones with these legends."
     },
     {
         title: 'Yosemite in the Mirror',
         imgSrc: '/static/images/gallery/yosemiteMirror.jpg',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "Yosemite National Park, CA",
+        date: "April 10, 2025",
+        description: "Leaving Yosemite just past Tunnel View towards the South Entrance. I could've framed this better."
     },
     {
         title: 'DJ Sam',
         imgSrc: '/static/images/gallery/samDJ.jpg',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "Skooliepalooza, Ehrenberg, AZ",
+        date: "January 25, 2025",
+        description: "I think Sam actually has a DJ name? Someone let me know. His set was fire at Skooliepalooza."
     },
     {
         title: 'J in SD',
         imgSrc: '/static/images/gallery/jInVan.png',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "San Diego, CA",
+        date: "April 2, 2025",
+        description: "A photo before we parted ways in San Diego (even tho we met back up in Yosemite shortly after)"
     },
     {
         title: 'Chicken Film',
         imgSrc: '/static/images/gallery/chickenFilm.jpg',
-        isVertical: false
+        isVertical: false,
+        camera: "Vivitar PS:35",
+        location: "Sleeping Giant, Kaua'i, HI",
+        date: "April 4, 2025",
+        description: "It's a chicken. On film."
     },
     
     
